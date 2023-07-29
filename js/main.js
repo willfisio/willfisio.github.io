02839bfd6ccf32b63be54241bb22cdf4ffce7969
William O. Souto <williamotavio.fisio@gmail.com>
2023-07-28 23:32:42 -0300

    up

--- js/main.js
+++ js/main.js
@@ -64,6 +64,31 @@
         loop: true,
         dots: false,
         nav : false,
+        center : true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+    $(".serviced-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        loop: false,
+        dots: false,
+        nav : false,
         responsive: {
             0:{
                 items:1
